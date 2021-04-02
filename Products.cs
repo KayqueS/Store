b1148b324d66c8b79c11492d3bc54bf4f258587d
KayqueS <kayque.sanmartin65@gmail.com>
2021-04-02 09:48:22 -0300

    Add project files.

---
+++ Products.cs
@@ -0,0 +1,29 @@
+﻿namespace Store
+{
+    class Products
+    {
+        public string Name;
+        public double Price;
+        public int Amount;
+
+        public double TotalValueInStock()
+        {
+            return Price * Amount;
+        }
+
+        public void AddProduct(int amount)
+        {
+            Amount += amount;
+        }
+
+        public void RemoveProduct(int amount)
+        {
+            Amount -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, $ {Price:F2}, {Amount} units, Total: $ {TotalValueInStock():F2}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Products prod = new Products();
+
+            Console.WriteLine("Enter product date:");
+            Console.Write("Name: ");
+            prod.Name = Console.ReadLine();
+            Console.Write("Price: ");
+            prod.Price = double.Parse(Console.ReadLine());
+            Console.Write("Amount in stock: ");
+            prod.Amount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"Product Data: {prod}");
+
+            Console.WriteLine();
+            Console.Write("Type the number of products to be added to stock: ");
+            int amt = int.Parse(Console.ReadLine());
+            prod.AddProduct(amt);
+
+            Console.WriteLine();
+            Console.WriteLine($"Updated Data: {prod}");
+
+            Console.Write("Type the number of products to be removed from stock: ");
+            amt = int.Parse(Console.ReadLine());
+            prod.RemoveProduct(amt);
+
+            Console.WriteLine();
+            Console.WriteLine($"Updated Data: {prod}");
+        }
+    }
+}
